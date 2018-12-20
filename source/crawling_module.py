f07062048860566f25002b20000c0e4f4e22d2af
cjsonghae <43847856+cjsonghae@users.noreply.github.com>
2018-12-20 14:38:55 +0900

    할리스. 커피빈 수정

--- source/crawling_module.py
+++ source/crawling_module.py
@@ -1,7 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-def coffeebeancrawling():
+def main():
+    # URL 데이터를 가져올 사이트 url 입력
+def coffeebean():
     url = "http://www.coffeebeankorea.com/promotion/list.asp"
     soup = urllib.request.urlopen(url).read()
     # URL 주소에 있는 HTML 코드를 soup에 저장합니다.
@@ -11,31 +13,37 @@ def coffeebeancrawling():
     list =[]
     # abc =[]
     for text in spans:
-            list.append(text.get_text())
-def hollyscrawling():
+        list.append(text.get_text())
+    # for i in range(0, 2):
+    #     print("이벤트" + str(i)+ list[i])
+    print(list)
+
+    coffeebean = ""
+    for i in range(0,len(list)):
+        coffeebean += "이벤트 " + str(i + 1) +": " + list[i].strip() + "\n"
+        # print("이벤트 : " + str(i+1) + list[i].strip() + "\n")
+
+    print(coffeebean)
+    return coffeebean
+
+def hollys()
     url = "http://www.hollys.co.kr/news/event/list.do"
     soup = urllib.request.urlopen(url).read()
     # URL 주소에 있는 HTML 코드를 soup에 저장합니다.
     soup = BeautifulSoup(soup, "html.parser")
     spans = soup.find_all("dl", class_="event_list")
-
     list = []
     abc = []
     for naver_text in spans:
         list.append(naver_text.get_text())
-
     for event in list:
         abc.append(event.strip().replace('\n', ''))
     print(abc)
+    hollys = ""
+    for i in range(0,len(abc)):
+        hollys += "이벤트 " + str(i +1) +": " + abc[i].strip() + "\n"
+    print(hollys)
+    return hollys
 
-    # for event in list:
-    #     abc.append(event.strip().replace('\n', '').replace('\r',''))
-    print(list)
-    # list = []
-    # abc =[]
-    # for naver_text in spans:
-    #    list.append(naver_text.get_text())
-    #
-    # for event in list:
-    #     abc.append(event.strip().replace('\n',''))
-    # print(abc)
+if __name__ == "__main__":
+    main()
